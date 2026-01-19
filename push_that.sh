5c24caa502175b9b3a6122a0a2290300f8fd924f
kopaba2 <badzaparfait2@gmail.com>
2026-01-19 23:50:41 +0100

    My secong repot

---
+++ push_that.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+message=$1
+
+if [ -z "$message" ]; then
+        echo "Le lancement du script doit être accompagné par une commentaire."
+        exit 1
+fi
+
+git add --all
+git commit -m "$message"
+git push origin
+
+
+
+git push origin
